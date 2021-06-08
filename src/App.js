415aed2ae3a1ab3ecda5c456243765b855ab8662
Samuel Abu <abu.sammie@gmail.com>
2021-06-08 22:22:01 +0200

    button techniques explored

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Button
-        size="large"
-        style={{
-          fontSize: 24,
-          borderRadius: 10,
-          boxShadow: 20
-        }} 
-        variant="contained" 
-        color="secondary" >
-        Hello World
-      </Button>
+        <ButtonGroup variant="contained" color="primary">
+          <Button
+            startIcon={<SaveIcon />}
+            size="large" 
+             >
+            Save
+          </Button>
+          <Button
+            startIcon={<DeleteIcon />}
+            size="large" >
+            Discard
+          </Button>
+        </ButtonGroup>
+      
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
