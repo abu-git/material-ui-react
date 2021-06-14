0a0dfb9faafdb59e35cb657775b91859a4dc144d
Samuel Abu <abu.sammie@gmail.com>
2021-06-14 15:55:52 +0200

    checkbox material-ui

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Checkbox />
         <ButtonGroup variant="contained" color="primary">
           <Button
             startIcon={<SaveIcon />}
-            size="large" 
-             >
+            size="large" >
             Save
           </Button>
           <Button
