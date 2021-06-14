06f7bb80aefa0da454eb54367bd9a3d4268f0fd5
Samuel Abu <abu.sammie@gmail.com>
2021-06-14 16:27:16 +0200

    checkbox explored

--- src/App.js
+++ src/App.js
@@ -6,12 +6,36 @@ import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+function CheckboxExample(){
+  const [checked, setChecked] = React.useState(true);
+  return(
+    <FormControlLabel
+      control={
+        <Checkbox 
+          checked={checked}
+          icon={<SaveIcon />}
+          checkedIcon={<SaveIcon />}
+          onChange={(e)=> setChecked(e.target.checked)}
+          inputProps={{
+            'aria-label' : 'secondary checkbox'
+          }} 
+        />
+      }
+      label="Testing Checkbox"
+    />
+      
+    
+      
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Checkbox />
+        <CheckboxExample />
         <ButtonGroup variant="contained" color="primary">
           <Button
             startIcon={<SaveIcon />}
